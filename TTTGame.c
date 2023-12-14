3faacc63edd1e4727e0694bcb6f542af3fc18495
jbreitsprecher <breitsprechet2002@gmail.com>
2023-12-14 16:51:44 -0600

    Functions created and conditions started

--- TTTGame.c
+++ TTTGame.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /*Initializing to '_' for "empty"*/
 char TL = '_';    //top left
@@ -13,7 +14,10 @@ char BL = '_';    //bottom left
 char BC = '_';    //bottom center
 char BR = '_';    //bottome right
 
+int not = 0;      //number of terms
+
 void PrintBoard();
+bool GameOver();
 
 void main()
 {
@@ -33,6 +37,13 @@ void main()
     //printf("%s", P2Name);
 
     PrintBoard();
+
+    printf("To play, please input the spot you would like to play. (TMB;LCR)");
+    
+    while(!GameOver())
+    {
+
+    }
 }
 
 void PrintBoard()
@@ -40,4 +51,31 @@ void PrintBoard()
     printf("%c %c %c\n",TL,TC,TR);
     printf("%c %c %c\n",ML,MC,MR);
     printf("%c %c %c\n",BL,BC,BR);
+}
+
+bool GameOver()
+{
+    //Checking for wins
+    if(not < 3)
+    {
+        return false;
+    }
+    else
+    {
+        // checks top row win
+        if( TL == TC && TL == TR && TC == TR && TR != '_' )
+        {
+            return true;
+        }
+        else if( ML == MC && ML == MR && MC == MR && MR != '_' )
+        {
+            return true;
+        }
+        else if( BL == BC && BL == BR && BC == BR && BR != '_' )
+        {
+            return true;
+        }
+        else if( TL == ML && BL == TL && ML == BL && TL != '_' )
+    }
+    return false;
 }
