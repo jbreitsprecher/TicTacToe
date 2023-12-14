0fcf8e4cd9df93faee7fa9ec8e2b1f8526e5b49c
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-12-14 17:06:46 -0500

    scanning name inputs

--- TTTGame.c
+++ TTTGame.c
@@ -2,5 +2,18 @@
 
 void main()
 {
+    char P1Name[] = "placeholder";
+    char P2Name[] = "placeholder";
+
     printf("\nLet's Play Tic-Tac-Toe!\n\n");
+
+    printf("Please Enter Player 1's name:\n");
+    scanf("%s", &P1Name);
+    printf("\n");
+
+    printf("Please Enter Player 2's name:\n");
+    scanf("%s", &P2Name);
+    printf("\n");
+
+    //printf("%s", P2Name);
 }
